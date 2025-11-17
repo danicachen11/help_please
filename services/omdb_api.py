7d3ab8c7f2c82e423a05ffc22bcc84317a3b1459
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-11-17 17:41:49 -0600

    Update omdb_api.py

--- services/omdb_api.py
+++ services/omdb_api.py
@@ -1,1 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OMDB_KEY = os.getenv("OMDB_KEY")
+
+def get_movie_details(title):
+    url = "http://www.omdbapi.com/"
+    params = {"t": title, "apikey": OMDB_KEY}
+
+    r = requests.get(url, params=params)
+    data = r.json()
+
+    if data.get("Response") == "True":
+        return {
+            "title": data["Title"],
+            "year": data["Year"],
+            "plot": data["Plot"],
+            "poster": data["Poster"]
+        }
+    return None
 
