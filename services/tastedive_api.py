989978d1abeebdf6b9ca85bb2d166056de0cbee6
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-11-17 17:41:38 -0600

    Update tastedive_api.py

--- services/tastedive_api.py
+++ services/tastedive_api.py
@@ -1,1 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TASTE_KEY = os.getenv("TASTEDIVE_KEY")
+
+def get_related_movies(query):
+    url = "https://tastedive.com/api/similar"
+
+    params = {
+        "q": query,
+        "type": "movies",   
+        "limit": 5,
+        "k": TASTE_KEY
+    }
+
+    r = requests.get(url, params=params)
+    results = r.json()
+
+    movie_list = [item["Name"] for item in results["Similar"]["Results"]]
+    return movie_list
 
